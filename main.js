d6a00987176c4b0d2a2794c3c8df69bdf7515fc1
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-05-26 23:46:59 -0300

    added changin idea functionality

--- main.js
+++ main.js
@@ -9,3 +9,28 @@ document.addEventListener("DOMContentLoaded", () => {
         textElement.appendChild(span);
     });
 });
+
+function rotateIdeas(){
+    let textHandle = document.getElementById("ideaHandle");
+    const ideasList = ["Criar uma textura de Minecraft",
+        "Abrir uma empresa de Design Gráfico",
+        "Criar um mod de Minecraft",
+        "Compor uma música",
+        "Abrir um canal no Youtube",
+        "Programar uma calculadora",
+        "Criar uma pokedéx online"];
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    let ideaIndex = getRandomInt(0, ideasList.length - 1);
+
+    textHandle.textContent = ideasList[ideaIndex];
+}
+
+rotateIdeas();
+
+// setInterval(rotateIdeas, 1000);
