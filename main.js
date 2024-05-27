00ecdbe044145d993afe97753a02dbb108735202
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-05-27 11:44:12 -0300

    added dark/light mode support and finished the ideas display

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+let prevIndex = -1;
 function rotateIdeas(){
     let textHandle = document.getElementById("ideaHandle");
     const ideasList = ["Criar uma textura de Minecraft",
@@ -19,18 +20,49 @@ function rotateIdeas(){
         "Abrir um canal no Youtube",
         "Programar uma calculadora",
         "Criar uma pokedéx online"];
-
+    
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        let newIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+        
+        while (newIndex === prevIndex) {
+            newIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+            console.log("repeated number!");
+        }
+        return newIndex;
     }
-    
-    let ideaIndex = getRandomInt(0, ideasList.length - 1);
 
-    textHandle.textContent = ideasList[ideaIndex];
+    let ideaIndex = getRandomInt(0, ideasList.length - 1);
+    prevIndex = ideaIndex;
+    
+    textHandle.classList.add("fadeOut");
+
+    setTimeout(function(){
+        textHandle.textContent = ideasList[ideaIndex];
+        textHandle.classList.remove("fadeOut");
+        textHandle.classList.add("fadeIn");
+    }, 500);
+        textHandle.classList.remove("fadeIn");
 }
 
 rotateIdeas();
 
-// setInterval(rotateIdeas, 1000);
+setTimeout(function(){
+    setInterval(rotateIdeas, 3000);
+}, 2000);
+
+function updateFavicon(){
+    const faviconLight = './favicon/favicon-light.ico';
+    const faviconDark = './favicon/favicon-dark.ico';
+    const favicon = document.getElementById("favicon");
+    
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        favicon.href = faviconDark;
+    } else {
+        favicon.href = faviconLight;
+    }
+}
+
+updateFavicon();
+window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', updateFavicon);
